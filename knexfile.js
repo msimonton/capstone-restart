b49bc7102f286323fce9c2e141593ee8d4214ebb
Marc Simonton <marcasimonton@gmail.com>
2017-10-27 12:24:36 -0600

    put in knexfile

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,8 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database:process.env.DATABASE_URL
+      database:'postgres://zcvjvhfjpvtpta:a78919810ec9c0868a61a05aa10f87e9af9f04e29878fd2f611a6ed00c568b4d@ec2-184-73-159-137.compute-1.amazonaws.com:5432/d3
+      04cb3ptr1poj'
     },
     pool: {
       min: 2,

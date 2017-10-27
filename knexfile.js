48e21bf2a901cb911715ca72ebdfa528cdb03360
Marc Simonton <marcasimonton@gmail.com>
2017-10-27 16:12:27 -0600

    slfdjsl

--- knexfile.js
+++ knexfile.js
@@ -12,9 +12,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database:'postgres://tbefngjvprrfvk:7685d75305632faa589d2aa9d9a90e4cee287b4c81580d3f17af9e07c1b34eeb@ec2-184-73-159-137.compute-1.amazonaws.com:5432/dc62joinnf9ni6?ssl=true'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10

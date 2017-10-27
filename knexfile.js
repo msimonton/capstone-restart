1f24db485df4f8cabe30d2eb9d5629df6d088541
Marc Simonton <marcasimonton@gmail.com>
2017-10-27 13:16:45 -0600

    lskj

--- knexfile.js
+++ knexfile.js
@@ -13,8 +13,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database:'postgres://zcvjvhfjpvtpta:a78919810ec9c0868a61a05aa10f87e9af9f04e29878fd2f611a6ed00c568b4d@ec2-184-73-159-137.compute-1.amazonaws.com:5432/d3
-      04cb3ptr1poj'
+      database:process.env.DATABASE_URL,
     },
     pool: {
       min: 2,
